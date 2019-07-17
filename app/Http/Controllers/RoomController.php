3d5f7862225fe02cee01cd92f668add8fa211cdc
Suguru Oki <oki.suguru@hamee.co.jp>
2019-07-18 08:46:26 +0900

    [修正] 諸々修正 PC以降のためにpush

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -33,24 +33,24 @@ class RoomController extends Controller
 
     public function store(RoomCreateRequest $request)
     {
-        $this->room->user_id = Auth::id();
-        $this->room->home_type = $request->home_type;
-        $this->room->room_type = $request->room_type;
-        $this->room->accommodate = $request->accommodate;
-        $this->room->bed_room = $request->bed_room;
-        $this->room->bath_room = $request->bath_room;
+        $this->room->user_id      = Auth::id();
+        $this->room->home_type    = $request->home_type;
+        $this->room->room_type    = $request->room_type;
+        $this->room->accommodate  = $request->accommodate;
+        $this->room->bed_room     = $request->bed_room;
+        $this->room->bath_room    = $request->bath_room;
         $this->room->listing_name = $request->listing_name;
-        $this->room->slug = str_slug($request->listing_name);
-        $this->room->summary = $request->summary;
-        $this->room->address = $request->address;
-        $this->room->is_tv = $request->get('is_tv') ? true : false;
-        $this->room->address = $request->address;
-        $this->room->is_kitchen = $request->is_kitchen ? true : false;
-        $this->room->is_heating = $request->is_heating ? true : false;
-        $this->room->is_internet = $request->is_internet ? true : false;
-        $this->room->is_air = $request->is_air ? true : false;
-        $this->room->active = $request->active ? true : false;
-        $this->room->price = $request->price;
+        $this->room->slug         = str_slug($request->listing_name);
+        $this->room->summary      = $request->summary;
+        $this->room->address      = $request->address;
+        $this->room->is_tv        = $request->get('is_tv') ? true : false;
+        $this->room->address      = $request->address;
+        $this->room->is_kitchen   = $request->is_kitchen ? true : false;
+        $this->room->is_heating   = $request->is_heating ? true : false;
+        $this->room->is_internet  = $request->is_internet ? true : false;
+        $this->room->is_air       = $request->is_air ? true : false;
+        $this->room->active       = $request->active ? true : false;
+        $this->room->price        = $request->price;
 
         if ($request->hasFile('images')) {
             $this->room->save();
